086824ae11b975e831f3139dff03c79d44da7ae1
Eu-Bin KIM <tlrndk123@gmail.com>
2021-06-26 16:15:47 +0900

    eval logic is done

--- wisdomify/paths.py
+++ wisdomify/paths.py
@@ -8,9 +8,13 @@ from os import path
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.__str__()
 DATA_DIR = path.join(PROJECT_ROOT, "data")
 
+
 # project - files
 CONF_JSON = path.join(PROJECT_ROOT, "conf.json")
 
 # data - files
 WISDOM2DEF_TSV = path.join(DATA_DIR, "wisdom2def.tsv")
 WISDOM2EG_TSV = path.join(DATA_DIR, "wisdom2eg.tsv")
+
+# data - trained models
+WISDOMIFIER_A_CKPT = path.join(DATA_DIR, "lightning_logs/version_0/checkpoints/wisdomify_def_epoch=19_train_loss=0.00.ckpt")
