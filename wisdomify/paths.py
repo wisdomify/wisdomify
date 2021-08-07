498154f4e98fbc7b679a6fe507a99b9e2edae33b
DicoTiar <mw18074@bristol.ac.uk>
2021-08-07 20:32:17 +0100

    [#43] paths.py updated

--- wisdomify/paths.py
+++ wisdomify/paths.py
@@ -9,16 +9,16 @@ from wisdomify.config import RUN_VER
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.__str__()
 DATA_DIR = path.join(PROJECT_ROOT, "data")
+
 # path를 지정하는 것은? 일단, tsv 한 이유? -> 단순하게.
 # tsv파일이 너무 불편하다! 그때는 DB로.
 WISDOMDATA_DIR = path.join(DATA_DIR, "wisdomdata")
+WISDOMDATA_VER_DIR = path.join(WISDOMDATA_DIR, f"version_{RUN_VER}")
+
 LIGHTNING_LOGS_DIR = path.join(DATA_DIR, "lightning_logs")
 
 # project - files
 CONF_JSON = path.join(PROJECT_ROOT, "conf.json")
-
-# data - files
-WISDOMDATA_VER_DIR = path.join(WISDOMDATA_DIR, f"version_{RUN_VER}")
 
 # the directories in which each version is stored
 WISDOMIFIER = path.join(LIGHTNING_LOGS_DIR, f"version_{RUN_VER}")
