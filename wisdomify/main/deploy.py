18ef689c225319f213e30aee4d8ed367b1c829c1
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-08-14 11:23:49 +0100

    [#52] debug mode removed

--- wisdomify/main/deploy.py
+++ wisdomify/main/deploy.py
@@ -223,4 +223,4 @@ def checkHealth():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0', threaded=True)
+    app.run(debug=False, port=5000, host='0.0.0.0', threaded=True)
