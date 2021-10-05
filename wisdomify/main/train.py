8142d56e5723e5d7e61d1fad14b98e55c6274e26
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-10-05 11:34:00 +0900

    [#71] checkpoint save format testing 1

--- wisdomify/main/train.py
+++ wisdomify/main/train.py
@@ -24,13 +24,12 @@ def main():
 
     # --- build an experiment instance --- #
     exp = Experiment.build(ver, device, wandb_support)
-    model_name = "wisdom"
+    model_name = "wisdomifier"
 
     # --- init callbacks --- #
     checkpoint_callback = ModelCheckpoint(
-        # f-string으로 적지 않으면 무조건 model_name-v1.ckpt로 저장된다. (v1을 없애기 위해서는 이렇게 작성해야한다.
-        filename=f"{model_name}ifier",
-        monitor=None,
+        filename=model_name,
+        save_top_k=1,
         verbose=False
     )
 
