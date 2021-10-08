bd20219906d9b95451375de2053f3af0cdafd959
ohsuz <gl_gd@naver.com>
2021-10-08 11:36:22 +0000

    [#79] fix encoding error:  I am a contributor~status

--- wisdomify/loaders.py
+++ wisdomify/loaders.py
@@ -3,5 +3,5 @@ from wisdomify.paths import CONF_JSON
 
 
 def load_conf() -> dict:
-    with open(CONF_JSON, 'r') as fh:
+    with open(CONF_JSON, 'r', encoding="utf-8") as fh:
         return json.loads(fh.read())
