38ed7019be3fc2d8b258a5e678ad645009835ab5
eubinecto <eubinecto>
2021-11-12 03:35:06 +0000

    [#98] fixed: main_infer.py

--- main_infer.py
+++ main_infer.py
@@ -26,7 +26,7 @@ def main():
     config = load_config()[model][ver]
     with connect_to_wandb(job_type="infer", config=config) as run:
         # --- init a wisdomifier --- #
-        flow = flows.ExperimentFlow(run, model, ver, device)("d")
+        flow = flows.ExperimentFlow(run, model, ver, device)("d", config)
         wisdomifier = Wisdomifier(flow.rd_flow.rd, flow.datamodule)
         # --- inference --- #
         print("### desc: {} ###".format(desc))
