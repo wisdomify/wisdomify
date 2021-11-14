c0e00b5794dd48c37b796f40b2ea1453a9831aaf
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-11-14 23:23:02 +0900

    [#104] apis implemented with some bugs

--- main_deploy.py
+++ main_deploy.py
@@ -1,8 +1,12 @@
 import argparse
 
 from flask import Flask
+from flask import jsonify, request
 
-from wisdomify.apis import WisdomifyView, StorytellView
+from wisdomify.apis import WisdomifierAPI, StorytellerAPI
+
+wisdomifier_api = WisdomifierAPI()
+storyteller_api = StorytellerAPI()
 
 app = Flask(__name__)
 
@@ -21,9 +25,34 @@ def check_params():
         raise ValueError("'--ver' should be stated for 'wisdomify' deployment.")
 
 
+@app.route('/wisdomify', methods=['GET'])
+def sents2wisdoms():
+    """
+    sents -> wisdoms
+    """
+    form = request.json
+    sent = form['sent']
+
+    return jsonify(wisdomifier_api.infer(sent))
+
+
+@app.route('/storytell', methods=['GET'])
+def wisdoms2sents():
+    """
+    wisdoms -> sents
+    """
+    form = request.json
+    sent = form['wisdom']
+
+    return jsonify(storyteller_api.infer(sent))
+
+
+@app.route('/healthz', methods=['GET'])
+def checkHealth():
+    return "Alive", 200
+
+
 if __name__ == '__main__':
     check_params()
 
-    StorytellView.register(app)
-    WisdomifyView.register(app)
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
--- wisdomify/apis.py
+++ wisdomify/apis.py
@@ -1,82 +1,75 @@
 import json
+import os
+from functools import reduce
 
 import requests as requests
-import torch
+from elasticsearch import Elasticsearch
+from flask import jsonify
+from transformers import BertTokenizerFast, AutoModelForMaskedLM
 
 from wisdomify.constants import (
     ES_USERNAME,
     ES_PASSWORD,
-    ES_CLOUD_ID,
-    WANDB_PROJECT, ROOT_DIR,
+    ES_CLOUD_ID, ROOT_DIR,
 )
-
-from elasticsearch import Elasticsearch
-
-from flask import jsonify, request
-from flask_classful import FlaskView, route
-
-from transformers import BertTokenizerFast, AutoConfig, AutoModelForMaskedLM, BertForMaskedLM, AutoTokenizer
-
 from wisdomify.docs import Story
 from wisdomify.loaders import load_config, load_device
-from wisdomify.models import RDBeta
-from wisdomify.tensors import WiskeysBuilder, Wisdom2SubwordsBuilder
-from wisdomify.wisdomifier import Wisdomifier
+from wisdomify.models import RDBeta, RD
+from wisdomify.tensors import WiskeysBuilder, Wisdom2SubwordsBuilder, Wisdom2EgInputsBuilder
 
 
 def get_wisdom():
     byte_json = requests.get(
-        'https://api.wandb.ai/artifactsV2/gcp-us/wisdomify/QXJ0aWZhY3Q6MzcwMjQ4NDQ=/36b09d5f21a92db67d6c099e93b07d8c')
-    return json.loads(byte_json.content)['data']
+        'https://api.wandb.ai/artifactsV2/gcp-us/wisdomify/QXJ0aWZhY3Q6MzcwMjQ4MTQ=/2b3ca62907826459a957675957540776')
+    return list(reduce(lambda i, j: i + j, json.loads(byte_json.content)['data']))
 
 
-class WisdomifyView(FlaskView):
-    """
-    sents -> wisdoms
-    """
-    config = load_config()['rd_beta']['b']
-    device = load_device(False)
-    wisdoms = get_wisdom()
+class WisdomifierAPI:
+    def __init__(self):
+        self.config = load_config()['rd_beta']['a']
+        self.device = load_device(False)
+        self.wisdoms = get_wisdom()
 
-    def index(self):
-        form = request.json
-        sent = form['sent']
+        self.tok_dir = os.path.join(ROOT_DIR, 'resource', 'tokenizer')
+        self.rd_ckpt_path = os.path.join(ROOT_DIR, 'resource', 'rd.ckpt')
 
-        bert_mlm = AutoModelForMaskedLM.from_pretrained(self.config['bert'])
-        tokenizer = BertTokenizerFast.from_pretrained(self.tok_dir_path)
+        self.bert_mlm = AutoModelForMaskedLM.from_pretrained(self.config['bert'])
+        self.tokenizer = BertTokenizerFast.from_pretrained(self.tok_dir)  # path
 
-        bert_mlm.resize_token_embeddings(len(tokenizer))
-        wiskeys = WiskeysBuilder(tokenizer, self.device)(self.wisdoms)
-        wisdom2subwords = Wisdom2SubwordsBuilder(tokenizer, self.config['k'], self.device)(self.wisdoms)
-        rd = RDBeta(bert_mlm, wisdom2subwords, wiskeys,
-                    self.config['k'], self.config['lr'], self.device)
+        self.bert_mlm.resize_token_embeddings(len(self.tokenizer))
+        self.wiskeys = WiskeysBuilder(self.tokenizer, self.device)(self.wisdoms)
+        self.wisdom2subwords = Wisdom2SubwordsBuilder(self.tokenizer, self.config['k'], self.device)(self.wisdoms)
+        self.rd = RDBeta.load_from_checkpoint(self.rd_ckpt_path,  # path
+                                              bert_mlm=self.bert_mlm,
+                                              wisdom2subwords=self.wisdom2subwords,
+                                              wiskeys=self.wiskeys,
+                                              device=self.device)
 
-        rd.load_state_dict(torch.load(self.rd_bin_path))
+        self.inputs_builder = Wisdom2EgInputsBuilder(self.tokenizer, self.config['k'], self.device)
 
-        datamodule = Wisdom2DefDataModule(self.config,
-                                          tokenizer,
-                                          self.wisdoms,
-                                          None,
-                                          self.device)
+    def infer(self, sent):
+        sents = [sent]
+        wisdom2sent = [("", sent) for sent in sents]
+        X = self.inputs_builder(wisdom2sent)
 
-        wisdomifier = Wisdomifier(rd, datamodule)
+        P_wisdom = self.rd.P_wisdom(X)
+        results = list()
+        for S_word_prob in P_wisdom.tolist():
+            wisdom2prob = [
+                (wisdom, prob)
+                for wisdom, prob in zip(self.wisdoms, S_word_prob)
+            ]
+            # sort and append
+            results.append(sorted(wisdom2prob, key=lambda x: x[1], reverse=True))
 
-        result = wisdomifier(sents=[sent])
-
-        return "Index"
+        return results
 
 
-class StorytellView(FlaskView):
-    """
-    wisdoms -> examples & definitions
-    """
+class StorytellerAPI:
+    def __init__(self):
+        self.es = Elasticsearch(ES_CLOUD_ID, http_auth=(ES_USERNAME, ES_PASSWORD))
 
-    def index(self):
-        es = Elasticsearch(ES_CLOUD_ID, http_auth=(ES_USERNAME, ES_PASSWORD))
-
-        form = request.json
-        wisdom = form['wisdom']
-
+    def infer(self, wisdom):
         query = {
             'match_phrase': {
                 'sents': {
@@ -98,10 +91,10 @@ class StorytellView(FlaskView):
         index_name = ",".join(Story.all_names())
         size = 10000
 
-        res = es.search(index=index_name,
-                        query=query,
-                        highlight=highlight,
-                        size=size)
+        res = self.es.search(index=index_name,
+                             query=query,
+                             highlight=highlight,
+                             size=size)
 
         parsed = [
             f"index: {hit['_index']}, highlight:{hit['highlight']['sents'][0]}"
