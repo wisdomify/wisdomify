d622f01cfc3f921b9bef6d6d2966b66b6a7c84db
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-11-15 20:54:56 +0900

    [#104] CORS error fixed, requirements updated(cors), return type updated (both)

--- main_deploy.py
+++ main_deploy.py
@@ -1,20 +1,22 @@
 import argparse
 
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 from wisdomify.apis import WisdomifyView, StorytellView
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app, resources={r"/*": {"origin": "*"}})
 
 
+@cross_origin(origin='*')
 @app.route('/healthz', methods=['GET'])
 def checkHealth():
     return "Alive", 200
 
 
 if __name__ == '__main__':
-
     StorytellView.register(app)
     WisdomifyView.register(app)
-    app.run(host='0.0.0.0', port=8080, debug=False, threaded=False)
-
+    app.run(host='0.0.0.0', port=8000, debug=False, threaded=False)
