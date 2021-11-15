2e70525fe644fd4bea93fcd17a02d45c8b581c31
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-11-15 13:48:04 +0900

    [#104] PR change requests applied, error page added for storytellView

--- wisdomify/apis.py
+++ wisdomify/apis.py
@@ -1,27 +1,20 @@
 import argparse
-import json
 
-import requests as requests
 from elasticsearch import Elasticsearch
 from flask import jsonify, request
 from flask_classful import FlaskView
 
 from wisdomify import flows
-from wisdomify.connectors import connect_to_wandb
+from wisdomify.connectors import connect_to_wandb, connect_to_es
 from wisdomify.constants import (
     ES_USERNAME,
     ES_PASSWORD,
     ES_CLOUD_ID,
 )
 from wisdomify.docs import Story
+from wisdomify.flows import SearchFlow
 from wisdomify.loaders import load_config, load_device
 from wisdomify.wisdomifier import Wisdomifier
-
-
-def get_wisdom():
-    byte_json = requests.get(
-        'https://api.wandb.ai/artifactsV2/gcp-us/wisdomify/QXJ0aWZhY3Q6MzcwMjQ4MTQ=/2b3ca62907826459a957675957540776')
-    return json.loads(byte_json.content)['data']
 
 
 class WisdomifyView(FlaskView):
@@ -65,41 +58,24 @@ class StorytellView(FlaskView):
     """
     wisdoms -> examples & definitions
     """
-    es = Elasticsearch(ES_CLOUD_ID, http_auth=(ES_USERNAME, ES_PASSWORD))
+    es = connect_to_es()
+
+    index_name = ",".join(name for name in Story.all_names())
+    size = 10000
 
     def index(self):
         form = request.json
         wisdom = form['wisdom']
 
-        query = {
-            'match_phrase': {
-                'sents': {
-                    'query': wisdom
-                }
-            }
-        }
-        highlight = {
-            'fields': {
-                'sents': {
-                    'type': 'plain',
-                    'fragment_size': 100,
-                    'number_of_fragments': 2,
-                    'fragmenter': 'span'
-                }
-            }
-        }
-
-        index_name = ",".join(Story.all_names())
-        size = 10000
-
-        res = self.es.search(index=index_name,
-                             query=query,
-                             highlight=highlight,
-                             size=size)
+        flow = SearchFlow(self.es, self.index_name, self.size)(wisdom)
+        res = flow.res
 
         parsed = [
             f"index: {hit['_index']}, highlight:{hit['highlight']['sents'][0]}"
             for hit in res['hits']['hits']
         ]
 
-        return jsonify(parsed)
+        if parsed:
+            return jsonify(parsed)
+        else:
+            return f"No data for '{wisdom}'", 404
