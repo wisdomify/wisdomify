e855bcaa54fbac175d8afc7718f3912d0b440b70
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-11-17 01:49:23 +0900

    [#111] Definition endpoint implemented

--- main_deploy.py
+++ main_deploy.py
@@ -2,12 +2,24 @@ import argparse
 
 from flask import Flask
 from flask_cors import CORS, cross_origin
+from flasgger import Swagger
 
 from wisdomify.apis import WisdomifyView, StorytellView
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app, resources={r"/*": {"origin": "*"}})
+
+app.config['SWAGGER'] = {
+    "swagger_version": "2.0",
+    "title": "Leaf",
+    "version": "0.0.1",
+    "headers": [
+
+    ],
+}
+
+swagger = Swagger(app)
 
 
 @cross_origin(origin='*')
