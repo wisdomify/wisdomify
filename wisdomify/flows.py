ba289f8a9057a2d2dc083ea039c69c55f3e6eed9
eubinecto <eubinecto>
2021-11-20 12:26:01 +0000

    [#115] fixed the tests, removed RDSimple (as it is incorrect)

--- wisdomify/flows.py
+++ wisdomify/flows.py
@@ -16,7 +16,7 @@ from more_itertools import chunked
 from tqdm import tqdm
 from transformers import BertTokenizerFast, AutoConfig, AutoModelForMaskedLM, BertForMaskedLM, AutoTokenizer
 from wandb.sdk.wandb_run import Run
-from wisdomify.models import RD, RDAlpha, RDBeta, RDGamma, RDGammaSync, RDSimple
+from wisdomify.models import RD, RDAlpha, RDBeta, RDGamma, RDGammaSync
 from wisdomify.tensors import Wisdom2SubwordsBuilder, WiskeysBuilder
 from wisdomify.connectors import connect_to_es
 from wisdomify.constants import WISDOMS_A, WISDOMS_B, WISDOM2QUERY_RAW_A, WISDOM2DEF_RAW_A, WISDOM2DEF_RAW_B, \
@@ -609,31 +609,6 @@ class RDGammaSyncFlow(RDFlow):
         return "rd_gamma_sync"
 
 
-class RDSimpleFlow(RDFlow):
-    def build_rd(self):
-        """
-        at this point, you have access to:
-        self.bert_mlm
-        self.tokenizer
-        self.device
-        self.wisdoms
-        self.wisdom2subwords
-        self.config
-        get use of these data to build your rd, and save to:
-        self.rd <= RDSomething(...)
-        """
-        self.rd = RDSimple(self.config['k'], self.config['lr'],
-                           self.bert_mlm, self.wisdom2subwords)
-
-    def load_rd(self):
-        self.rd = RDSimple.load_from_checkpoint(self.rd_ckpt_path,
-                                                bert_mlm=self.bert_mlm,
-                                                wisdom2subwords=self.wisdom2subwords)
-
-    @property
-    def name(self):
-        return "rd_simple"
-
 # ======= experiment flows ======= #
 # this is here to prevent circular import error
 from wisdomify.datamodules import WisdomifyDataModule, Wisdom2EgDataModule, Wisdom2DefDataModule # noqa
@@ -676,8 +651,6 @@ class ExperimentFlow(TwoWayFlow):
             self.rd_flow = RDGammaFlow(self.run, self.ver)
         elif self.model == "rd_gamma_sync":
             self.rd_flow = RDGammaSyncFlow(self.run, self.ver)
-        elif self.model == "rd_simple":
-            self.rd_flow = RDSimpleFlow(self.run, self.ver)
         else:
             raise ValueError(f"Invalid model:{self.model}")
 
